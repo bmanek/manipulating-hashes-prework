f0797895df7381932ba8372e86d660228cb3f38f
Ben Manekin <ben.manekin@gmail.com>
2019-02-07 17:20:20 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -9,7 +9,7 @@ def second_challenge
   #code your solution here!
   
   groceries.each do |categories, items|
-    categories.each do |items|
+    categories.values do |items|
       puts "#{items}"
     end
   end
